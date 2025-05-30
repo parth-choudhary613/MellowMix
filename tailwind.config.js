b2082c4d06fad76caca7aa32288512ffbb279812
Parth Choudhary <parthchoudhary4372@gmail.com>
2025-05-30 18:05:09 +0530

    adding older version of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
